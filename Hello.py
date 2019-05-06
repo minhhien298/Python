8bf847ae4fdb07ebbed165617757a6095a0fbcfb
Le Ha Minh Hien <minhhien90@gmail.com>
2019-05-06 20:53:59 +0700

    Create Hello.py

---
+++ Hello.py
@@ -0,0 +1,9 @@
+import datetime
+
+age = 10
+name = 'Cuomg'
+print(f"{name} is {age} years old")
+ketqua = 1 + 90
+print(ketqua)
+datetime.datetime.now()
+print(datetime.datetime(2019,5,6,19,49,36,841292))
