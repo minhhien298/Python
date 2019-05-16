f2e898f213f0f0f6d8960dffccc9e6749c5bd75f
Le Ha Minh Hien <minhhien90@gmail.com>
2019-05-16 17:42:51 +0700

    Create Guiemail.py

---
+++ Guiemail.py
@@ -0,0 +1,29 @@
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+fromaddr = "hienlhm@saomaisoft.com"
+toaddr = "minhhien90@gmail.com"
+msg = MIMEMultipart()
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = "TIÊU ĐỀ CỦA MAIL (SUBJECT)"
+body = "NỘI DUNG MAIL"
+try:
+    msg.attach(MIMEText(body, 'plain'))
+    filename = './input/melbourne-housing-snapshot/melb_data.csv'
+    attachment = open(filename, "rb")
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    msg.attach(part)
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(fromaddr, "epN*TCwd")
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
+except Exception as e:
+ print(str(e))
