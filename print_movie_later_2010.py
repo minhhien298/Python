794acd5cb7ccf9fbaf01db2089fde9f91ef90ca2
Le Ha Minh Hien <minhhien90@gmail.com>
2019-05-19 16:34:59 +0700

    dsd

--- print_movie_later_2010.py
+++ print_movie_later_2010.py
@@ -1,6 +1,7 @@
 import csv
 
 with open('movies.csv') as csv_file:
+with open('sample/movies.csv', encoding="utf8") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
