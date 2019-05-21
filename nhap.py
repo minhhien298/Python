9f549c81561bd37e079040f15079d5b5166efe98
Le Ha Minh Hien <minhhien90@gmail.com>
2019-05-21 09:32:07 +0700

    123

--- nhap.py
+++ nhap.py
@@ -4,30 +4,37 @@ with open('sample/movies.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
-        if line_count > 0:
-            film = row[1]
-            if film.endswith(" "):
-                year = film[-6:-2]
-            else:
-                year = film[-5:-1]
-            try:
-                if int(year) >= 2010:
-                    print(f'{row[0]} - {row[1]} - {row[2]}.')
-                    pass
-            except ValueError:
-                #print(f"{line_count} - {film}")
-                pass
-        line_count += 1
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            genres = row[2].split('|')
+            genre_set |= set(genres)
+            line_count += 1
+
+    listphim = list(genre_set)
+    print(listphim)
     print(f'Processed {line_count} lines.')
 
+def checklist(T):
+ if 'Thriller'in T:
+     return True
+
+listso = []
+
+
+
+counts = 0
+with open('sample/movies.csv', encoding="utf8") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+     if(checklist(row[2].split('|'))== True ):
+            counts += 1
+    listso.append(counts)
+    print(listso)
+
+#     listso.append(str(sum(checklist(str(row[2].split('|'))) == True for row in csv_reader)))
 
 
 
 
-
-
-
-    # luc phim theo nam
-    # cac phim khong lap lai
-    # the loai phim nao co nhieu phim nhat
-    # 8gio sang tong hop tin gui
