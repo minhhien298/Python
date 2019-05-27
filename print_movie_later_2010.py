2a033c64cd5816df0ed4f5c7a4448791235794f3
Le Hien <minhhien90@gmail.com>
2019-05-27 18:55:49 +0700

    Update print_movie_later_2010.py

--- print_movie_later_2010.py
+++ print_movie_later_2010.py
@@ -1,11 +1,8 @@
 import csv
 
-<<<<<<< HEAD
-with open('sample/movies.csv') as csv_file:
-=======
 
 with open('sample/movies.csv', encoding="utf8") as csv_file:
->>>>>>> 57e9312495c19ad96ca8854812fb4ff0f4712470
+
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
