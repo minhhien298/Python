8d0b380dcec0a9616929fbf7305f5b1d21e56939
Le Ha Minh Hien <minhhien90@gmail.com>
2019-07-16 21:55:25 +0700

    ok

---
+++ baivenha7.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+def zigzag(n):
+    def move(i, j):
+        if j < (n - 1):
+            return max(0, i - 1), j + 1
+        else:
+            return i + 1, j
+
+    a = [[0] * n for _ in range(n)] #tao ma tran vuong n*n toan` 0
+    x, y = 0, 0
+    for v in range(n * n):
+        a[y][x] = v
+        if (x + y) & 1:
+            x, y = move(x, y)
+        else:
+            y, x = move(y, x)
+    return a
+
+import re
+def no_accent_vietnamese(s):
+    s = re.sub(r'[àáạảãâầấậẩẫăằắặẳẵ]', 'a', s)
+    s = re.sub(r'[ÀÁẠẢÃĂẰẮẶẲẴÂẦẤẬẨẪ]', 'A', s)
+    s = re.sub(r'[èéẹẻẽêềếệểễ]', 'e', s)
+    s = re.sub(r'[ÈÉẸẺẼÊỀẾỆỂỄ]', 'E', s)
+    s = re.sub(r'[òóọỏõôồốộổỗơờớợởỡ]', 'o', s)
+    s = re.sub(r'[ÒÓỌỎÕÔỒỐỘỔỖƠỜỚỢỞỠ]', 'O', s)
+    s = re.sub(r'[ìíịỉĩ]', 'i', s)
+    s = re.sub(r'[ÌÍỊỈĨ]', 'I', s)
+    s = re.sub(r'[ùúụủũưừứựửữ]', 'u', s)
+    s = re.sub(r'[ƯỪỨỰỬỮÙÚỤỦŨ]', 'U', s)
+    s = re.sub(r'[ỳýỵỷỹ]', 'y', s)
+    s = re.sub(r'[ỲÝỴỶỸ]', 'Y', s)
+    s = re.sub(r'[Đ]', 'D', s)
+    s = re.sub(r'[đ]', 'd', s)
+    return s
+
+if __name__ == "__main__":
+    print(np.matrix(zigzag(10)))
+    print("Nhap tieng viet di ")
+    a = input()
+    print(no_accent_vietnamese(a))
